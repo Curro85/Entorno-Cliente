a7d44c9331d90327af25f1cc5c977d0c7a6b8c86
Curro85 <alexdaw995@gmail.com>
2024-10-04 09:02:30 +0200

    Bloque 1

--- Bloque1.js
+++ Bloque1.js
@@ -1,3 +1,62 @@
 // Ejercicio 1.1
 let str = "Buenas";
 console.log(str);
+
+// Ejercicio 1.2
+let numero = 24;
+console.log(numero);
+
+// Ejercicio 1.3
+let mentira = false;
+console.log(mentira);
+
+// Ejercicio 1.4
+let vacio = null;
+let und;
+console.log(vacio + " " + und);
+
+// Ejercicio 1.5
+let item = {
+    name: "coche",
+    price: 12500,
+    category: "sedan"
+}
+console.log(item);
+
+// Ejercicio 1.6
+let ciudades = ["Jerez", "Puerto", "Cádiz", "Linares"];
+console.log(ciudades);
+
+// Ejercicio 1.7
+let colorFav = "Azul";
+console.log(colorFav);
+colorFav = "Verde";
+console.log(colorFav);
+
+// Ejercicio 1.8
+let noValor;
+let nulo = null;
+console.log(noValor + " " + nulo);
+
+// Ejercicio 1.9
+let varOut = "Fuera";
+function prueba() {
+    let varIn = "Dentro";
+    console.log(varOut + " " + varIn);
+};
+prueba();
+//console.log(varOut + " " + varIn);
+// Comento la linea anterior para poder ejecutar los demás ejercicios
+
+// Ejercicio 1.10
+let _variable = 1;
+let $variable = 2;
+let variable = 3;
+let Variable = 4;
+//let 0variable = 5;
+//let var = 6;
+console.log(_variable + " " + $variable + " " + variable + " " + Variable);
+
+// Ejercicio 1.11
+let num1 = 4;
+let num2 = 2;
