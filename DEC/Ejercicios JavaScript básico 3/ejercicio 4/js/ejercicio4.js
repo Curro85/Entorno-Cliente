01edb1b7810ecc4197c13d3de80b3e77d58d91bb
Curro85 <alexdaw995@gmail.com>
2024-10-18 10:18:19 +0200

    Ejercicios 18/10/24

---
+++ DEC/Ejercicios JavaScript básico 3/ejercicio 4/js/ejercicio4.js
@@ -0,0 +1,8 @@
+function toggleBox() {
+    const caja = document.getElementById('box');
+    const boton = document.getElementById('toggleBtn');
+
+    caja.style.display = 'none';
+    boton.textContent = "Mostrar Caja";
+
+}   
