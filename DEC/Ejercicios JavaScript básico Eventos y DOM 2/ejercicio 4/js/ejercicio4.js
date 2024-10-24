6b107bbdf1ebd9a9f64f3c2a018ed229aa1e60b6
Curro85 <alexdaw995@gmail.com>
2024-10-24 12:46:31 +0200

    Corregido

--- DEC/Ejercicios JavaScript básico Eventos y DOM 2/ejercicio 4/js/ejercicio4.js
+++ DEC/Ejercicios JavaScript básico Eventos y DOM 2/ejercicio 4/js/ejercicio4.js
@@ -2,14 +2,12 @@ function toggleBox() {
     const caja = document.getElementById('box');
     const boton = document.getElementById('toggleBtn');
 
-    boton.onclick = function () {
+    if (boton.textContent == "Ocultar Caja") {
+        caja.style.display = "none";
+        boton.textContent = "Mostrar Caja";
+    } else {
+        caja.style.display = "block";
+        boton.textContent = "Ocultar Caja";
+    }
 
-        if (boton.textContent == "Ocultar Caja") {
-            caja.style.display = "none";
-            boton.textContent = "Mostrar Caja";
-        } else {
-            caja.style.display = "block";
-            boton.textContent = "Ocultar Caja";
-        }
-    };
 }   
