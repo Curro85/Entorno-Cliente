b9e9133784488c587ed6b456736556ff6913e78d
Curro85 <alexdaw995@gmail.com>
2025-02-19 12:31:16 +0100

    Paginas empezadas

---
+++ DEC/Proyecto Tarot/js/compatibilidad.js
@@ -0,0 +1,15 @@
+const botones = document.getElementsByClassName('zodiac-section');
+
+function listar() {
+    for (let i = 0; i < botones.length; i++) {
+        console.log(botones[i].value);
+    }
+}
+
+function marcar_signo() {
+    
+}
+
+function compatibilidad() {
+
+}
